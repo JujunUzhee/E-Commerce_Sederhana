d657c06f30ea4420ea5b57d9e645bddde30ddc02
JujunUzhee <jujunowh675@gmail.com>
2025-11-12 17:08:05 +0700

    update size teks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,10 @@ const Header = ({ handleOpenModalCart }) => {
       <header className="fixed top-0 z-50 w-full bg-sky-600 shadow">
         <div className="mx-auto px-4">
           <div className="flex h-20 items-center justify-between">
-            <h1 className="text-xl font-bold text-gray-100 md:ml-16 md:text-2xl lg:text-3xl">
+            <h1 className="text-xs font-bold text-gray-100 md:ml-16 md:text-2xl lg:text-3xl">
               E-Commerce
             </h1>
-            <div className="flex h-full items-center justify-between gap-1 md:gap-7">
+            <div className="flex h-full items-center justify-between gap-2 md:gap-7">
               <button
                 type="button"
                 className="relative rounded-full bg-sky-700 p-2 text-gray-100"
